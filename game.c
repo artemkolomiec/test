780a832d15ad8bdeecb2b6a673fba0548f2b1187
Artem <101944364+artemkolomiec@users.noreply.github.com>
2023-07-27 11:17:21 +0300

    Create game.c

---
+++ game.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void playSnakeGame() {
+    // Code for the snake game goes here
+    std::cout << "Playing Snake Game..." << std::endl;
+    // ...
+}
+
+int main() {
+    // Call the function to play the snake game
+    playSnakeGame();
+    
+    return 0;
+}
